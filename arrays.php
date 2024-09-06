03c47da699c26f72f75dc0ec8745467381b05c2b
FlipSkateHd <flipskatehd@gmail.com>
2024-09-06 17:29:30 -0300

    🎉commit inicial

---
+++ arrays.php
@@ -0,0 +1,8 @@
+<?php
+ $notas = array("Gabriel" => 6, "Mauá" => 8, "Kauã" => 4);  //Declarando array
+ echo "As notas dos alunos foram, respectivamente: ";
+ foreach ($notas as &$alunos) {   //Utilizando laço para exibir os valores
+    echo "\n $alunos \n";
+}
+
+?>
---
+++ olaMundo.php
@@ -0,0 +1,7 @@
+<?php
+$mundo = "mundo"; //Definindo variáveis
+define ("linguagem", "PHP"); //Definindo constantes, constantes não são mutáveis
+
+echo "Olá " . $mundo . "! \n" . "Fui feito em: " . linguagem;
+
+?>  
