e857b4a6a64aa189b5b6a1db9899ce0177d43791
FELIPE NOGUEIRA GOMES <felipe_n_gomes@estudante.sesisenai.org.br>
2024-10-04 12:20:53 -0300

    update dos sites

--- arrays.php
+++ arrays.php
@@ -1,7 +1,7 @@
 <?php
- $notas = array("Gabriel" => 6, "Mauá" => 8, "Kauã" => 4);  //Declarando array
- echo "As notas dos alunos foram, respectivamente: ";
- foreach ($notas as &$alunos) {   //Utilizando laço para exibir os valores
+$notas = array("Gabriel" => 6, "Mauá" => 8, "Kauã" => 4);  //Declarando array
+echo "As notas dos alunos foram, respectivamente: ";
+foreach ($notas as &$alunos) {   //Utilizando laço para exibir os valores
     echo "\n $alunos \n";
 }
 
